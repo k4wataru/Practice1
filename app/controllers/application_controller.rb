5d7b3a9f878e24080dc3965bbd32792c367cc2f6
k4wataru <k.wataru1234@gmail.com>
2024-06-02 16:29:36 +0000

    Practice

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,21 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
   def after_sign_in_path_for(resouce)
-   about_path
+   post_images_path
   end
   # after_sign_in_path_forはdeviceのメソッド サインイン後の遷移先を指定できる
   # (resouce)という引数はログインを実行したモデル(User)のインスタンスが格納
   # ログイン後ユーザーに関連するページに遷移させたい時はresouceを活用
-  
+
   def after_sign_out_path_for(resouce)
     about_path
   end
-  
+
   protected
-  
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
-  
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,8 @@ ActiveRecord::Schema.define(version: 2024_06_01_100835) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -35,13 +35,13 @@ ActiveRecord::Schema.define(version: 2024_06_01_100835) do
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
+    t.integer "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "post_images", force: :cascade do |t|
-    t.string "shop_image"
+    t.string "shop_name"
     t.text "caption"
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
